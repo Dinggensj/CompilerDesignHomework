359665516d7ea97c02cbe7381b30faa6a684dbdf
Dingge_nsj <dinggensj@gmail.com>
2026-03-11 14:20:44 +0800

    homework2

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -1,5 +1,16 @@
-#include<stdio.h>
-int main(){
-    printf("Hello, World!");
+#include <iostream>
+#include <map>
+using namespace std;
+
+multimap<string, string> Map;
+string str;
+
+int main() {
+    cin >> str;
+    cout << str;
+    // for (auto &p : Map) {
+    //     cout << p.first << " => " << p.second << endl;
+    // }
+
     return 0;
 }
